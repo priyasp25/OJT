dbff9100fb04e27d2deb31ef3cdf2f4724603b05
priyasp25 <125352860+priyasp25@users.noreply.github.com>
2023-07-07 15:39:48 +0530

    PHP(database)

---
+++ 6july/Conn.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database="class";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+
+
+?>
